01bea269ac8f95f615fe1416710b1f538a909b8a
ShashiS32 <46977833+ShashiS32@users.noreply.github.com>
2023-12-28 17:28:23 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import random
+
+randominte = random.randint(1 , 10)
+print (randominte)
+
+random_flooat = random.random()
+
+
+
+print (random_flooat * 5)
+
+love = random.randint (1,100)
+
+print (love)
