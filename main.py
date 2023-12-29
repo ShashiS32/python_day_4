e956d3b5647de11cc6b48945a79c58fb37511c64
ShashiS32 <46977833+ShashiS32@users.noreply.github.com>
2023-12-29 20:03:13 +0000

    finished RPS

--- main.py
+++ main.py
@@ -1,45 +1,38 @@
 import random
 
 while True:
+    choices = ["rock", "paper", "scissors"]
 
-  choices = ["Rock" , "Paper" , "Scissors"]
-  
-  player_choice = input("Rock, Paper, or Scissors?: ")
-  
-  
-  AIRandom = random.randint(0,2)
-  
-  AIChoice = choices[AIRandom]
-  
-  if player_choice.lower == "rock":
-    if AIChoice == "Rock":
-      print ("You tied with the AI!")
-    elif AIChoice == "Paper":
-      print ("You lost, the AI chose paper!")
+    player_choice = input("rock, paper, or scissors?: ")
+
+    ai_random = random.randint(0, 2)
+    ai_choice = choices[ai_random]
+
+    if player_choice.lower() == "rock":
+        if ai_choice == "rock":
+            print("You tied with the AI!")
+        elif ai_choice == "paper":
+            print("You lost, the AI chose paper!")
+        else:
+            print("You won, the AI chose scissors!")
+    elif player_choice.lower() == "paper":
+        if ai_choice == "rock":
+            print("You won, the AI chose rock!")
+        elif ai_choice == "paper":
+            print("You tied with the AI!")
+        else:
+            print("You lost, the AI chose scissors!")
+    elif player_choice.lower() == "scissors":
+        if ai_choice == "rock":
+            print("You lost, the AI chose rock!")
+        elif ai_choice == "paper":
+            print("You won, the AI chose paper!")
+        else:
+            print("You tied with the AI!")
     else:
-      print("You won, the AI chose scissors!")
-  elif player_choice.lower == "paper":
-    if AIChoice == "Rock":
-      print ("You won, the AI chose rock!")
-    elif AIChoice == "Paper":
-      print ("You tied with the AI!")
-    else:
-      print("You lost, the AI chose scissors!")
-  elif player_choice.lower =="scissors":
-    if AIChoice == "Rock":
-      print ("You lost, the AI chose rock!")
-    elif AIChoice == "Paper":
-      print ("You won, the AI chose paper!")
-    else:
-      print("You tied with the AI!")
-  
-  else:
-    print("Invalid choice!")
-  
-  play_again = input("Do you want to play again? (yes/no): ")
-  if play_again.lower() == "no":
-      print("Thanks for playing!")
-      break
-  
-  
-  
+        print("Invalid choice!")
+
+    play_again = input("Do you want to play again? (yes/no): ")
+    if play_again.lower() == "no":
+        print("Thanks for playing!")
+        break
