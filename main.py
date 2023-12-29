ceae9fe053ed039997d75e6de9e92b9d2d7e0c79
ShashiS32 <46977833+ShashiS32@users.noreply.github.com>
2023-12-29 19:50:24 +0000

    fixed rock paper scissors

--- main.py
+++ main.py
@@ -1,31 +1,45 @@
 import random
 
-choices = ["Rock" , "Paper" , "Scissors"]
+while True:
 
-player_choice = input("Rock, Paper, or Scissors? (1 for Rock, 2 for Paper, 3 for Scissors): ")
-
-AIRandom = random.randint(0,3)
-
-AIChoice = choices[AIRandom]
-
-if player_choice == "1":
-  if AIChoice == 0:
-    print ("You tied with the AI!")
-  elif AIChoice == 1:
-    print ("You lost, the AI chose paper!")
+  choices = ["Rock" , "Paper" , "Scissors"]
+  
+  player_choice = input("Rock, Paper, or Scissors?: ")
+  
+  
+  AIRandom = random.randint(0,2)
+  
+  AIChoice = choices[AIRandom]
+  
+  if player_choice.lower == "rock":
+    if AIChoice == "Rock":
+      print ("You tied with the AI!")
+    elif AIChoice == "Paper":
+      print ("You lost, the AI chose paper!")
+    else:
+      print("You won, the AI chose scissors!")
+  elif player_choice.lower == "paper":
+    if AIChoice == "Rock":
+      print ("You won, the AI chose rock!")
+    elif AIChoice == "Paper":
+      print ("You tied with the AI!")
+    else:
+      print("You lost, the AI chose scissors!")
+  elif player_choice.lower =="scissors":
+    if AIChoice == "Rock":
+      print ("You lost, the AI chose rock!")
+    elif AIChoice == "Paper":
+      print ("You won, the AI chose paper!")
+    else:
+      print("You tied with the AI!")
+  
   else:
-    print("You won, the AI chose scissors!")
-elif player_choice == "2":
-  if AIChoice == 0:
-    print ("You won, the AI chose rock!")
-  elif AIChoice == 1:
-    print ("You tied with the AI!")
-  else:
-    print("You lost, the AI chose scissors!")
-else:
-  if AIChoice == 0:
-    print ("You lost, the AI chose rock!")
-  elif AIChoice == 1:
-    print ("You won, the AI chose paper!")
-  else:
-    print("You tied with the AI!")
+    print("Invalid choice!")
+  
+  play_again = input("Do you want to play again? (yes/no): ")
+  if play_again.lower() == "no":
+      print("Thanks for playing!")
+      break
+  
+  
+  
