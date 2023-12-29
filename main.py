78e8dfab164a62466b62c67f10aee7e9367ab220
ShashiS32 <46977833+ShashiS32@users.noreply.github.com>
2023-12-29 17:47:31 +0000

    Created a Rock Paper Scissors game

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 import random
 
-print ("Welcome to the coin simulation, heads = 1, tails = 0")
+choices = ["Rock" , "Paper" , "Scissors"]
 
-coin = random.randint(0,1)
+player_choice = input("Rock, Paper, or Scissors? (1 for Rock, 2 for Paper, 3 for Scissors): ")
 
-if coin == 1:
-  print ("The coin landed on heads")
-else: 
-  print ("The coin landed on tails")
+AIRandom = random.randint(0,3)
 
+AIChoice = choices[AIRandom]
+
+if player_choice == "1":
+  if AIChoice == 0:
+    print ("You tied with the AI!")
+  elif AIChoice == 1:
+    print ("You lost, the AI chose paper!")
+  else:
+    print("You won, the AI chose scissors!")
+elif player_choice == "2":
+  if AIChoice == 0:
+    print ("You won, the AI chose rock!")
+  elif AIChoice == 1:
+    print ("You tied with the AI!")
+  else:
+    print("You lost, the AI chose scissors!")
+else:
+  if AIChoice == 0:
+    print ("You lost, the AI chose rock!")
+  elif AIChoice == 1:
+    print ("You won, the AI chose paper!")
+  else:
+    print("You tied with the AI!")
